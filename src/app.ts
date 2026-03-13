3e3b2ec794f586ec7c105230c7e42898d5010406
sajib925 <sajibahmed1004@gmail.com>
2026-03-13 23:01:11 +0600

    update app file

--- src/app.ts
+++ src/app.ts
@@ -8,23 +8,26 @@ import { globalErrorHandler } from "./middlewares/globalErrorHandle.js";
 
 const app: Express = express();
 
-
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://next-portfolio-backend-navy.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
-  }),
-)
+  })
+);
+
+app.options("*", cors()); 
 
 app.use(cookieParser());
-
 app.use(compression());
 app.use(express.json());
 
 app.use("/api/v1", apiRoutes);
 
-app.use(notFound) 
-
+app.use(notFound);
 app.use(globalErrorHandler);
 
 export default app;
