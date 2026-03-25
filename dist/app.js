126a496683ea3c97ba17fe5d4fd7d0a67d0509a1
sajib925 <sajibahmed1004@gmail.com>
2026-03-25 18:00:09 +0600

    cors error

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-import express, {} from "express";
-import compression from "compression";
-const app = express();
-app.use(compression());
-app.use(express.json());
-app.get("/", (_req, res) => {
-    res.send("API is running");
-});
-export default app;
